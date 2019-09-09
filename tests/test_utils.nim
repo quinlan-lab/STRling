4f58857ac347acc17622609d02b7f8fdb993b78b
Harriet Dashnow <h.dashnow@gmail.com>
2019-09-09 16:06:05 -0600

    caculate local median_depth for each bound

--- tests/test_utils.nim
+++ tests/test_utils.nim
@@ -7,4 +7,7 @@ suite "utils suite":
     var targets = @[Target(name: "chr1", tid: 0, length: 10000)]
     check get_tid("chr1", targets) == 0
 
-
+  test "test median":
+    check median_depth(@[1,2,2]) == 2
+    check median_depth(@[2000]) == 1047 #Values greater than 1047 will be set to 1047 to avoid overflow
+    #echo median_depth(@[-11,2,2]) # This will throw an inded out of range error
