ddb96aebb6792df8b75d45f247b07abff50ce37a
Harriet Dashnow <h.dashnow@gmail.com>
2019-10-10 15:07:01 -0600

    fix genotyper spanning_read_est test

--- tests/test_genotyper.nim
+++ tests/test_genotyper.nim
@@ -12,4 +12,9 @@ suite "genotyper suite":
     Support(SpanningFragmentLength: 0, SpanningReadRepeatCount: 9, SpanningReadCigarInsertionLen: 0, SpanningReadCigarDeletionLen: 2, repeat: "AT"),
     ]
 
-    check spanning_read_est(reads) == Evidence(class: "spanning reads", repeat: "AT", allele1_bp: 0.0, allele2_bp: -2.0, allele1_ru: 10.0, allele2_ru: 9.0)
+    var est = spanning_read_est(reads)
+    check est.repeat == "AT"
+    check est.allele1_bp == 0.0
+    check est.allele2_bp == -2.0
+    check est.allele1_ru == 10.0
+    check est.allele2_ru == 9.0
